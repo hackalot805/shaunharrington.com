d875bd5ef8114dcb1884e723cf9dec4f56a4b375
hackalot805 <shaun@planetharrington.com>
2018-03-03 13:39:54 -0800

    fixes typo

--- src/photos.js
+++ src/photos.js
@@ -2,8 +2,11 @@ import "babel-polyfill";
 import React, { Component } from 'react';
 import sc_photo from './assets/sc.jpg';
 import sb_photo from './assets/sb.jpg';
+import sb2_photo from './assets/sb2.jpg';
 import './about.css';
 import { Carousel } from 'react-bootstrap';
+import './photos.css';
+
 /*
                       <div className="container-fluid">
                          <div className="row">
@@ -22,38 +25,30 @@ class Photos extends Component {
        <div>
           <div className="container-fluid text-left">
              <div className="row">
-                <div className='col-xs-10  col-xs-offset-1 '>
-                   <h1>Rides</h1>
-                   <section>
-                      <p>
-                         Once upon a time I was crazy for slammed hot rod v-twins...
-                      </p>
-
-                      <Carousel>
-                         <Carousel.Item>
-                            <img alt="900x500" src={sc_photo} />
-                            <Carousel.Caption>
-                               <h3>First slide label</h3>
-                               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                            </Carousel.Caption>
-                         </Carousel.Item>
-                         <Carousel.Item>
-                            <img alt="900x500" src={sb_photo} />
-                            <Carousel.Caption>
-                               <h3>Second slide label</h3>
-                               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </Carousel.Caption>
-                         </Carousel.Item>
-                         <Carousel.Item>
-                            <img  alt="900x500" src={sc_photo} />
-                            <Carousel.Caption>
-                               <h3>Third slide label</h3>
-                               <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                            </Carousel.Caption>
-                         </Carousel.Item>
-                      </Carousel>
-
-                   </section>
+                <div className='col-xs-12'>
+                   <Carousel>
+                      <Carousel.Item>
+                         <img alt="900x500" src={sc_photo} />
+                         <Carousel.Caption>
+                            <h3>SOULCRUSHER</h3>
+                            <p>My daily ride for about a decade.</p>
+                         </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                         <img  alt="900x500" src={sb2_photo} />
+                         <Carousel.Caption>
+                            <h3>SUPERBEAST</h3>
+                            <p>Sick body work with molded taillights, and even a retracting license plate.</p>
+                         </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                         <img alt="900x500" src={sb_photo} />
+                         <Carousel.Caption>
+                            <h3>SUPERBEAST</h3>
+                            <p>Why do people keep looking at me?</p>
+                         </Carousel.Caption>
+                      </Carousel.Item>
+                   </Carousel>
                 </div>
              </div>
           </div>
