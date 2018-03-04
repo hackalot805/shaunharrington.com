1aba031f6362bfcaf7f68c05157a5bd5539689dd
hackalot805 <shaun@planetharrington.com>
2018-03-03 17:29:08 -0800

    photos

--- src/photos.js
+++ src/photos.js
@@ -1,8 +1,13 @@
 import "babel-polyfill";
 import React, { Component } from 'react';
 import sc_photo from './assets/sc.jpg';
+import sc2_photo from './assets/sc2.jpg';
+import sc3_photo from './assets/sc3.jpg';
+import sc4_photo from './assets/sc4.jpg';
 import sb_photo from './assets/sb.jpg';
 import sb2_photo from './assets/sb2.jpg';
+import sb3_photo from './assets/sb3.jpg';
+import buddy_photo from './assets/buddy.jpg';
 import './about.css';
 import { Carousel } from 'react-bootstrap';
 import './photos.css';
@@ -35,17 +40,52 @@ class Photos extends Component {
                          </Carousel.Caption>
                       </Carousel.Item>
                       <Carousel.Item>
-                         <img  alt="900x500" src={sb2_photo} />
+                         <img alt="900x500" src={sc2_photo} />
+                         <Carousel.Caption>
+                            <h3>SOULCRUSHER</h3>
+                            <p>With the original tank and frontend.</p>
+                         </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                         <img alt="900x500" src={sc3_photo} />
+                         <Carousel.Caption>
+                            <h3>SOULCRUSHER</h3>
+                            <p>Sturgis style freedom.</p>
+                         </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                         <img alt="900x500" src={sc4_photo} />
+                         <Carousel.Caption>
+                            <h3>SOULCRUSHER</h3>
+                            <p>Arizona bar hopping.</p>
+                         </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                         <img  alt="900x500" src={sb_photo} />
+                         <Carousel.Caption>
+                            <h3>SUPERBEAST</h3>
+                            <p>Bit of a shop project.</p>
+                         </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                         <img alt="900x500" src={sb2_photo} />
                          <Carousel.Caption>
                             <h3>SUPERBEAST</h3>
                             <p>Sick body work with molded taillights, and even a retracting license plate.</p>
                          </Carousel.Caption>
                       </Carousel.Item>
                       <Carousel.Item>
-                         <img alt="900x500" src={sb_photo} />
+                         <img alt="900x500" src={sb3_photo} />
                          <Carousel.Caption>
                             <h3>SUPERBEAST</h3>
                             <p>Why do people keep looking at me?</p>
+                         </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                         <img alt="900x500" src={buddy_photo} />
+                         <Carousel.Caption>
+                            <h3>BUDDY</h3>
+                            <p>My dog is a nut case!</p>
                          </Carousel.Caption>
                       </Carousel.Item>
                    </Carousel>
