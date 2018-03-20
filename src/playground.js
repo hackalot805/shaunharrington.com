d875be06020aef7ab7ebe670fbd632b8d67b0e37
hackalot805 <shaun@planetharrington.com>
2018-03-20 14:35:21 -0700

    Adds playground

---
+++ src/playground.js
@@ -0,0 +1,14 @@
+import "babel-polyfill";
+import React, { Component } from 'react';
+
+class Playground extends Component {
+   render() {
+      return (
+       <div>
+          <iframe src="http://grafana.shaunharrington.com:3000/d-solo/PfsxDnkiz/gunner-dashboard?orgId=1&panelId=2&from=1521555938118&to=1521577538118" width="450" height="200" frameborder="0"></iframe>
+       </div>
+      );
+   }
+}
+
+export default Playground;
