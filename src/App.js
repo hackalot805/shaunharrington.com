ad5086f529bc3759434fa4dafcdacb53d3bb1c8d
hackalot805 <shaun@planetharrington.com>
2019-11-23 09:37:05 -0800

    Updates to scroll to top when navigation is used.

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
    };
 
    setPage = (key, page) => {
+      window.scrollTo(0,0);
       this.navItems = this.navItems.map((item) => {
          if (item.divider) {
             return item;
