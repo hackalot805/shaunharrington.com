4c4c44e3915b456b3e93d861252153f998c01acd
Shaun Harrington <shaun@planetharrington.com>
2025-08-01 12:19:13 -0700

    Update ExtractTranslations.ts

--- .github/scripts/ExtractTranslations.ts
+++ .github/scripts/ExtractTranslations.ts
@@ -1,4 +1,12 @@
-    const fs = require('fs');
-    const JSONData = process.env.JSON_DIFF; // Relative to the working directory
-//    const fileContent = fs.readFileSync(filePath, 'utf8');
-    console.log(JSONData);
+const fs = require('fs');
+const JSONData = process.env.JSON_DIFF;
+console.log(JSONData);
+
+const patternJS = /_js\([^)]*\)/;
+const patternP = /___p\([^)]*\)/;
+
+const matchJS = patternJS.exec(JSONData);
+const matchP = patternP.exec(JSONData);
+
+console.log(matchJS, matchP);
+// Use regex to exctract `_js` and `___p` instances.
