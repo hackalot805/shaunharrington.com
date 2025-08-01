add0e6b4e884213f66f132f4559330c1cde71909
Shaun Harrington <shaun@planetharrington.com>
2025-08-01 12:52:03 -0700

    Update ExtractTranslations.ts

--- .github/scripts/ExtractTranslations.ts
+++ .github/scripts/ExtractTranslations.ts
@@ -1,10 +1,6 @@
 const fs = require('fs');
 const JSONData = process.env.JSON_DIFF;
-console.log(JSONData);
-// const currentTranslations = process.env.CURRENT_TRANSLATIONS
-
-const currentTranslations = fs.readFileSync('./current_translations.json').toString();
-console.log(currentTranslations);
+const currentTranslations = JSON.parse(fs.readFileSync('./current_translations.json').toString());
 
 const patternJS = /_js\([^)]*\)/;
 const patternP = /___p\([^)]*\)/;
@@ -13,4 +9,17 @@ const matchJS = patternJS.exec(JSONData);
 const matchP = patternP.exec(JSONData);
 
 console.log(matchJS, matchP);
-// Use regex to exctract `_js` and `___p` instances.
+
+let newStrings = [];
+matchJS.foreach((match) => {
+  if (!(match in currentTranslations.translations)) {
+    newStrings.push(match);
+  }
+});
+matchP.foreach((match) => {
+  if (!(match in currentTranslations.translations)) {
+    newStrings.push(match);
+  }
+});
+
+console.log(newStrings);
