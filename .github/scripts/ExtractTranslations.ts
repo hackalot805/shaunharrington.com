e79be594d38ba40ca44e9780ce2e069e927283eb
Shaun Harrington <shaun@planetharrington.com>
2025-08-01 16:10:24 -0700

    Update ExtractTranslations.ts

--- .github/scripts/ExtractTranslations.ts
+++ .github/scripts/ExtractTranslations.ts
@@ -7,6 +7,7 @@ let newStrings = [];
 const patternJS = /_js\([^)]*\)/g;
 const matchJS = JSONData.match(patternJS);
 matchJS?.forEach((match) => {
+  console.log(match);
   // TODO: match.length - 2 is wrong, we might be passing params
   //       Find the char after the first param, if ')' we are good to go, if ',' we need to truncate and replace with ')'
   const token = match.substring(5, match.length - 2);
