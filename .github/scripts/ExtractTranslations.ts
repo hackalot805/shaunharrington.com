1df9f6bf78ceeb1c65ce104df054257776206832
Shaun Harrington <shaun@planetharrington.com>
2025-08-01 14:23:32 -0700

    Update ExtractTranslations.ts

--- .github/scripts/ExtractTranslations.ts
+++ .github/scripts/ExtractTranslations.ts
@@ -11,13 +11,13 @@ const matchP = JSONData.match(patternP);
 let newStrings = [];
 matchJS?.forEach((match) => {
   const token = match.substring(5, match.length - 2);
-  if (!(token in currentTranslations.translations) && !newStrings.find(match)) {
+  if (!(token in currentTranslations.translations) && !newStrings.includes(match)) {
     newStrings.push(match);
   }
 });
 matchP?.forEach((match) => {
   const token = match.substring(6, match.length - 2);
-  if (!(token in currentTranslations.translations) && !newStrings.find(match)) {
+  if (!(token in currentTranslations.translations) && !newStrings.includes(match)) {
     newStrings.push(match);
   }
 });
