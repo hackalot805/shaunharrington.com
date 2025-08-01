f3f15dc0329c7d2a85feba5cc5d0452e09025cbc
Shaun Harrington <shaun@planetharrington.com>
2025-08-01 13:39:54 -0700

    Update ExtractTranslations.ts

--- .github/scripts/ExtractTranslations.ts
+++ .github/scripts/ExtractTranslations.ts
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const JSONData = fs.readFileSync('../../diff.json');
+const JSONData = fs.readFileSync('../../diff.json').toString();
 const currentTranslations = JSON.parse(fs.readFileSync('./current_translations.json').toString());
 
 console.log(JSONData);
