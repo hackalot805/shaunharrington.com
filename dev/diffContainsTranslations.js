ca25fdaef6f79bbc861a1a6e851d5e78278ae233
Shaun Harrington <shaun@planetharrington.com>
2025-08-02 09:57:16 -0700

    Create diffContainsTranslations.js

---
+++ dev/diffContainsTranslations.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const JSONData = fs.readFileSync('../../diff.json').toString();
+
+const patternJS = /_js\([^)]*\)/g;
+const matchJS = JSONData.match(patternJS);
+const patternP = /___p\([^)]*\)/g;
+const matchP = JSONData.match(patternP);
+
+if (matchJS.length || matchP.length) {
+   console.log('true');
+}
+console.log('false');
