e07bf4a1e4d63b8f85a78bfa7ae73336744d62d9
Shaun Harrington <shaun@planetharrington.com>
2025-08-01 17:42:15 -0700

    Update testAction.ts

--- src/testAction.ts
+++ src/testAction.ts
@@ -2,18 +2,31 @@ const _js = (value: string): string {
    return value;
 }
 
-      let junk = _js('junk edited');
-      const testing = true ? _js('true junk') : _js('false junk');
-      junk = _js('junk new');
-      junk = _js('junk new');
-      const testing = true ? _js('true junk') : _js('false junk');
-      junk = _js('Time Required');
-      junk = _js('Guide');
-      junk = _js('Search for a %1');
+// Simple
+let junk = _js('junk simple');
 
-      // TODO: DOUBLE QUOTES ISN"T WORKING YET
+// Duplicated
+junk = _js('junk dublicated');
+junk = _js('junk duplicated');
 
-      // TODO: __p ISN"T WORKING YET
-      // junk = __p(1, '%1 day ago', '%1 days ago', `1`);
-      // junk = _js('%1 day ago');
-      // junk = _js('%1 days ago');
+// Two on one line
+let testing = true ? _js('junk one') : _js('junk two');
+
+// Two duplicated on one line 
+testing = true ? _js('true junk') : _js('false junk');
+
+// With params
+junk = _js('Search for a %1', 'tool');
+junk = _js('Search for a %1 and %2', 'tool', 'manual');
+
+// Found - These should not be included in the comment.
+junk = _js('Time Required');
+junk = _js('Guide');
+
+
+// TODO: DOUBLE QUOTES ISN'T WORKING YET
+
+// TODO: __p ISN"T WORKING YET
+// junk = __p(1, '%1 day ago', '%1 days ago', `1`);
+// junk = _js('%1 day ago');
+// junk = _js('%1 days ago');
