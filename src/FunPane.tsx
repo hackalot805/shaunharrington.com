81ee6d3b9813f62cf1b6e40504a052661954d24c
Shaun Harrington <shaun@planetharrington.com>
2025-08-01 17:35:18 -0700

    Update FunPane.tsx

--- src/FunPane.tsx
+++ src/FunPane.tsx
@@ -1,3 +1,4 @@
+
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import React, { Component } from 'react';
@@ -7,31 +8,12 @@ interface FunPaneProps {
    size: number
 }
 
-const _js = (value: string): string {
-   return value;
-}
-// Random Comment #8
 class FunPane extends Component<FunPaneProps> {
    render() {
       let { extra } = this.props;
 
-      let junk = _js('junk edited');
-      const testing = true ? _js('true junk') : _js('false junk');
-      junk = _js('junk new');
-      junk = _js('junk new');
-      const testing = true ? _js('true junk') : _js('false junk');
-      junk = _js('Time Required');
-      junk = _js('Guide');
-      junk = _js('Search for a %1', 'tool');
-
-      // TODO: DOUBLE QUOTES ISN"T WORKING YET
-      
-      // TODO: __p ISN"T WORKING YET
-      // junk = __p(1, '%1 day ago', '%1 days ago', `1`);
-      // junk = _js('%1 day ago');
-      // junk = _js('%1 days ago');
-      
       let cssClass = `fun-pane ${extra}`;
+
       const divStyle = {
          width: `80px`,
          height: `80px`,
